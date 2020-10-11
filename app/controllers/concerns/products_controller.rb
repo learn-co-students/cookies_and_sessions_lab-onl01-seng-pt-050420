e0a3a5317d8dae0941fa1022390bc33a5191edb2
ryancdavey1 <ryancdavey1@gmail.com>
2020-10-11 17:34:15 -0600

    Done.

--- app/controllers/concerns/products_controller.rb
+++ app/controllers/concerns/products_controller.rb
@@ -3,14 +3,7 @@ class ProductsController < ApplicationController
   end
 
   def add
-    # Get the item from the path
-    @item = Item.find(params[:id])
-   
-    # Load the cart from the session, or create a new empty cart.
-    cart = session[:cart] || []
-    cart << @item.id
-   
-    # Save the cart in the session.
-    session[:cart] = cart
+    cart << params[:product]
+    render :index
   end
 end
